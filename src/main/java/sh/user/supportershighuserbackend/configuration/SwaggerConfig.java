b50c5015470dd55aa498346c332b9a8a2357b52e
JayEsEichi <wlstpgns51@naver.com>
2025-09-19 02:28:45 +0900

    Swagger ㅑãimport 수정

--- src/main/java/sh/user/supportershighuserbackend/configuration/SwaggerConfig.java
+++ src/main/java/sh/user/supportershighuserbackend/configuration/SwaggerConfig.java
@@ -4,7 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springdoc.core.GroupedOpenApi;
+
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
