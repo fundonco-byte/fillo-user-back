5f3ef151206128ddb267fba3a04061de48a58faf
JayEsEichi <wlstpgns51@naver.com>
2025-09-19 02:45:29 +0900

    swagger 그냥 주석 처리

--- src/main/java/sh/user/supportershighuserbackend/configuration/SwaggerConfig.java
+++ src/main/java/sh/user/supportershighuserbackend/configuration/SwaggerConfig.java
@@ -1,34 +1,34 @@
-package sh.user.supportershighuserbackend.configuration;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
-@OpenAPIDefinition(
-        info = @Info(title = "온누리 API 정의서",
-                description = "전자제품 판매 온라인 사이트 온누리",
-                version = "v3"))
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public GroupedOpenApi chatOpenApi() {
-        String[] paths = {"/admin/**", "/api/**"};
-
-        return GroupedOpenApi.builder()
-                .group("온누리 API v3")
-                .pathsToMatch(paths)
-                .build();
-    }
-}
+//package sh.user.supportershighuserbackend.configuration;
+//
+//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+//import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+//import io.swagger.v3.oas.annotations.info.Info;
+//import io.swagger.v3.oas.annotations.security.SecurityScheme;
+//
+//import org.springdoc.core.models.GroupedOpenApi;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@SecurityScheme(
+//        name = "Bearer Authentication",
+//        type = SecuritySchemeType.HTTP,
+//        bearerFormat = "JWT",
+//        scheme = "bearer"
+//)
+//@OpenAPIDefinition(
+//        info = @Info(title = "온누리 API 정의서",
+//                description = "전자제품 판매 온라인 사이트 온누리",
+//                version = "v3"))
+//@Configuration
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public GroupedOpenApi chatOpenApi() {
+//        String[] paths = {"/admin/**", "/api/**"};
+//
+//        return GroupedOpenApi.builder()
+//                .group("온누리 API v3")
+//                .pathsToMatch(paths)
+//                .build();
+//    }
+//}
